7216d300fbfd48a97aadc76b0ce2697ab44e259f
Klazen <cmurphy1337@live.com>
2018-09-20 17:45:52 -0500

    mxGraph import and setup
    install mxGraph
    Import mxTypes from https://github.com/gooddaytoday/mxgraph-typescript-definitions
    configure initial load process to load mxGraph sources on init according to https://itnext.io/how-to-integrate-mxgraph-with-angular-6-18c3a2bb8566

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'app';
+  
+  @ViewChild('graphContainer') graphContainer: ElementRef;
+  
+  ngAfterViewInit() {
+    console.log("after init")
+    const graph = new mxGraph(this.graphContainer.nativeElement);
+
+    try {
+      const parent = graph.getDefaultParent();
+      graph.getModel().beginUpdate();
+
+      const vertex1 = graph.insertVertex(parent, '1', 'Vertex 1', 0, 0, 200, 80);
+      const vertex2 = graph.insertVertex(parent, '2', 'Vertex 2', 0, 0, 200, 80);
+
+      graph.insertEdge(parent, '', '', vertex1, vertex2);
+    } finally {
+      graph.getModel().endUpdate();
+      new mxHierarchicalLayout(graph).execute(graph.getDefaultParent());
+    }
+  }
 }
---
+++ src/assets/js/mxgraph.conf.js
@@ -0,0 +1,4 @@
+//Put mxGraph configuration here - things that you want set before inclusion of mxGraph
+//https://jgraph.github.io/mxgraph/docs/tutorial.html#2.1
+//https://itnext.io/how-to-integrate-mxgraph-with-angular-6-18c3a2bb8566
+mxBasePath = 'assets/mxgraph';
